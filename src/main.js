64ed71e02ce20f2828e9b7dd5ca6870de92c38a3
LuisFVR <vluiz4349@gmail.com>
2023-11-16 20:32:11 -0600

    Catching Estatico

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import '/src/js/app.js';
+import '/sw.js';
 
 import router from './router';
 
--- sw.js
+++ sw.js
@@ -1,7 +1,56 @@
-// sw.js
+
+// self.addEventListener('install', (event) => {
+//   event.waitUntil(
+//     caches.open('my-cache').then((cache) => {
+//       return cache.addAll([
+//         '/index.html',
+//         'src/main.js',
+//         'src/components/Sidebar.vue',
+//         'src/images/h1.png',
+//         'src/images/h2.jpg',
+//         'src/images/h4.jpg',
+//         'src/images/h5.jpg',
+//         'src/images/h7.jpg',
+//         'src/images/h8.jpg',
+//         'src/images/h9.jpg',
+//         'src/images/h10.jpg',
+//         'src/images/h11.jpg',
+//         'src/images/p1.jpg',
+//         'src/images/p2.jpg',
+//         'src/images/p3.jpg',
+//         'src/images/p4.jpg',
+//         'src/images/p5.jpg',
+//         'src/images/p6.jpg',
+//         'src/images/p7.jpg',
+//         'src/images/p8.jpg',
+//         'src/router/index.js',
+//         'src/views/About.vue',
+//         'src/views/Home.vue',
+//         'src/views/Login.vue',
+//         'src/views/Personas.vue',
+//         'src/App.vue',
+//         'src/style.css',
+//       ]);
+//     })
+//   );
+// });
+
+// self.addEventListener('fetch', (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then((response) => {
+//       return response || fetch(event.request).catch((error) => {
+//         console.error('Error al recuperar:', error);
+//       });
+//     })
+//   );
+// });
+
+//Cache 
+const CACHE_NAME = 'my-cache';
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('my-cache').then((cache) => {
+    caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll([
         '/index.html',
         'src/main.js',
@@ -30,6 +79,8 @@ self.addEventListener('install', (event) => {
         'src/views/Personas.vue',
         'src/App.vue',
         'src/style.css',
+        '/public/firebase-messaging-sw.js',
+        '/public/cara-feliz.png'
       ]);
     })
   );
@@ -37,8 +88,15 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+    caches.match(event.request).then((staticResponse) => {
+      // Buscar en la caché estática primero
+      return staticResponse || fetch(event.request).then((dynamicResponse) => {
+        // Si no se encuentra en la caché estática, intentar buscar en la red y cachear dinámicamente
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, dynamicResponse.clone());
+          return dynamicResponse;
+        });
+      });
     })
   );
 });
